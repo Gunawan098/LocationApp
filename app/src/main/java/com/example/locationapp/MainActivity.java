ee17c88580b8594c04e923a251a5f695a82ec83b
Gunawan098 <45507175+Gunawan098@users.noreply.github.com>
2020-10-13 21:56:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.locationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TRACKING_LOCATION_KEY = "tracking_location";
+    private TextView txtLocation;
+    private Button btnLocation;
+    private ImageView mAndroidImageView;
+
+    private boolean mTrackingLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+
+    private AnimatorSet mRotateAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtLocation = findViewById(R.id.textview_location);
+        btnLocation = findViewById(R.id.button_location);
+        mAndroidImageView = findViewById(R.id.imageview_android);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate);
+        mRotateAnim.setTarget(mAndroidImageView);
+
+        if (savedInstanceState != null) {
+            mTrackingLocation = savedInstanceState.getBoolean(TRACKING_LOCATION_KEY);
+        }
+
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Toggle the tracking state.
+             * @param v The track location button.
+             */
+            @Override
+            public void onClick(View v) {
+                if (!mTrackingLocation) {
+                    startTrackingLocation();
+                } else {
+                    stopTrackingLocation();
+                }
+            }
+        });
+
+        mLocationCallback = new LocationCallback() {
+            /**
+             * This is the callback that is triggered when the
+             * FusedLocationClient updates your location.
+             * @param locationResult The result containing the device location.
+             */
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (mTrackingLocation) {
+                    new FetchAddressTask(MainActivity.this, MainActivity.this).execute(locationResult.getLastLocation());
+                }
+            }
+        };
+    }
+
+    private void startTrackingLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            mTrackingLocation = true;
+            mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+
+            txtLocation.setText("address");
+            btnLocation.setText("Stop Tracking Location");
+            mRotateAnim.start();
+        }
+    }
+
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            mTrackingLocation = false;
+            btnLocation.setText("Start Tracking Location");
+            txtLocation.setText("address");
+            mRotateAnim.end();
+        }
+    }
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(TRACKING_LOCATION_KEY, mTrackingLocation);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startTrackingLocation();
+                } else {
+                    Toast.makeText(this, "Access Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onTaskCompleted(String result) {
+        if (mTrackingLocation) {
+            txtLocation.setText(result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mTrackingLocation) {
+            stopTrackingLocation();
+            mTrackingLocation = true;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mTrackingLocation) {
+            startTrackingLocation();
+        }
+        super.onResume();
+    }
+}
